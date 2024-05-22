96f722c44ce0640bc035f7c636b69c9955a0af87
Aayush701 <ayushghimire7011@gmail.com>
2024-05-22 19:22:10 +0545

    the usermanagement system backend implementation

---
+++ config/config.js
@@ -0,0 +1,10 @@
+const sessionSecret = "mysitesessionsecret";
+
+const emailUser = "ayushghimire225@gmail.com";
+const emailPassword = "YOUR_SMTP_PASSWORD";
+
+module.exports = {
+  sessionSecret,
+  emailUser,
+  emailPassword,
+};
