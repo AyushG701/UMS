d706880aef3a4707c97ac97eecaf63763ed7507e
Aayush701 <ayushghimire70111@gmail.com>
2024-07-18 21:54:44 +0545

    removing the deprecated elements

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/user_management_system");
+
+mongoose.connect("mongodb://127.0.0.1:27017/user_management_system", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Get the default connection
+const db = mongoose.connection;
+
+// Bind connection to error event (to get notification of connection errors)
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+// Bind connection to open event (once the connection is established)
+db.once("open", function () {
+  console.log("Connected to MongoDB successfully!");
+});
 
 const express = require("express");
 const app = express();
 
 //for user routes
-const userRoute = require('./routes/userRoute');
-app.use('/',userRoute);
-
+const userRoute = require("./routes/userRoute");
+app.use("/", userRoute);
 
 //for admin routes
-const adminRoute = require('./routes/adminRoute');
-app.use('/admin',adminRoute);
+const adminRoute = require("./routes/adminRoute");
+app.use("/admin", adminRoute);
 
-app.listen(3000,function(){
-    console.log("Server is runnnig...");
+app.listen(3000, function () {
+  console.log("Server is runnnig...");
 });
